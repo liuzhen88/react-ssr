8677d2b3376fccf44d5cac61638f91f7a182c408
liuzhen88 <1458861320@qq.com>
2017-07-17 20:24:45 +0800

    add index ssr

--- pages/about.js
+++ pages/about.js
@@ -3,23 +3,15 @@ import { render } from 'react-dom';
 import fetch from 'isomorphic-unfetch';
 
 class About extends React.Component{
-	static async getInitialProps({req, res}) {
-		const response = await fetch('http://120.25.152.42:4000/getTravel');
-		const data = await response.json();
+	static getInitialProps() {	 
 		return {
-			msg: req ? '这是服务器端渲染的，可以看看没有ajax请求':'这是客户端渲染的',
-			data:data
+			msg:'这是SPA单页面about路由'
 		}
 	}
     render(){
         return (
         	<div>
-            	<div>about page from {this.props.msg}{this.props.code}</div>
-        		{
-        			this.props.data.TrafficStations.map(function(item){
-        				return <div key={item.Id}>{item.Name}</div>
-        			})
-        		}
+            	<div>about page from {this.props.msg}</div>
         	</div>
         )
     }
--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,29 @@ import { render } from 'react-dom';
 import Link from 'next/link';
 
 class Index extends React.Component{
+	static async getInitialProps({req, res}) {
+		const response = await fetch('http://120.25.152.42:4000/getTravel');
+		const data = await response.json();
+		return {
+			msg: req ? '这是服务器端渲染的，可以看看没有ajax请求':'这是客户端渲染的',
+			data:data
+		}
+	}
     render(){
         return (
             <div>
-                <span>hello world next.js</span>
+                <div>hello world next.js</div>
                 <Link href='/about'>
                 	<a>to about</a>
                 </Link>
+                <div>========================================</div>
+                <div>about page from {this.props.msg}{this.props.code}</div>
+                <div>========================================</div>
+                {
+        			this.props.data.TrafficStations.map(function(item){
+        				return <div key={item.Id}>{item.Name}</div>
+        			})
+        		}
             </div>
         )
     }
